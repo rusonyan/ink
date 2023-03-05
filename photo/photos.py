bebb3d5ba1c5ebe936f4bf5309cc3b4e55fd444b
闫瑞松 <2011046760@qq.com>
2023-03-05 14:30:55 +0800

    发版4

--- photo/photos.py
+++ photo/photos.py
@@ -10,7 +10,7 @@ def handle():
     photo_page = PHOTO_ALBUM[random.randint(0, len(PHOTO_ALBUM) - 1)]
     logger.success('即将展示{}'.format(photo_page))
     shutil.copy(os.path.join(os.getcwd(), 'static', 'photo', photo_page),
-                os.getcwd() + '\\out.jpg')
+                os.path.join(os.getcwd(), 'out.jpg'))
 
 
 if __name__ == "__main__":
